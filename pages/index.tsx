2e43503ec8fe7c3954c9121d8faeb7209a243e2d
Ben Attanasio <benattanasio95@gmail.com>
2023-11-06 21:14:26 -0500

    Slight customization

--- pages/index.tsx
+++ pages/index.tsx
@@ -165,7 +165,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Legal Docs
+            Chat With Your PDF Docs
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -320,7 +320,7 @@ export default function Home() {
         </div>
         <footer className="m-auto p-4">
           <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+            Powered by LangChain, Pinecone, and OpenAI. Demo built by Mayowaoshin (Twitter: @mayowaoshin), customizations by Ben Attanasio (Twitter: @benattanasio).
           </a>
         </footer>
       </Layout>
